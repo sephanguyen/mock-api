4a2e940a57308a6be99fe35b88c011e2ec433a04
Pha Nguyen <pha.nguyen@aux.dkatalis.com>
2019-11-07 13:05:33 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.post("/customers/:customerId/cif", (req, res) =>
   res.json({ data: { cif: "8a8e876f6e43792c016e43a65a6900da" } })
